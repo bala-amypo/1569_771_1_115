f13bed0427e7f86d681fb420e3a46b89240a6137
bala-amypo <bala@amypo.in>
2025-12-15 10:45:45 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/TemperatureRule.java
@@ -0,0 +1,8 @@
+package com.example.demo.entity;
+
+public class TemperatureRule {
+     private long id;
+     private long shipmentId;
+     private String sensorId;
+     private 
+}
--- src/main/java/com/example/demo/entity/TemperatureSensorLog.java
+++ src/main/java/com/example/demo/entity/TemperatureSensorLog.java
@@ -6,5 +6,67 @@ public class TemperatureSensorLog {
        private long id;
        private long shipmentId;
        private String sensorId;
-       private LocalDateTime 
+       private LocalDateTime recordedAt;
+       private Double temperatureValue;
+       private String location;
+
+       public TemperatureSensorLog(){}
+
+    public TemperatureSensorLog(long shipmentId, String sensorId, LocalDateTime recordedAt, Double temperatureValue,
+            String location) {
+        this.shipmentId = shipmentId;
+        this.sensorId = sensorId;
+        this.recordedAt = recordedAt;
+        this.temperatureValue = temperatureValue;
+        this.location = location;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public long getShipmentId() {
+        return shipmentId;
+    }
+
+    public String getSensorId() {
+        return sensorId;
+    }
+
+    public LocalDateTime getRecordedAt() {
+        return recordedAt;
+    }
+
+    public Double getTemperatureValue() {
+        return temperatureValue;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setShipmentId(long shipmentId) {
+        this.shipmentId = shipmentId;
+    }
+
+    public void setSensorId(String sensorId) {
+        this.sensorId = sensorId;
+    }
+
+    public void setRecordedAt(LocalDateTime recordedAt) {
+        this.recordedAt = recordedAt;
+    }
+
+    public void setTemperatureValue(Double temperatureValue) {
+        this.temperatureValue = temperatureValue;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    
 }
