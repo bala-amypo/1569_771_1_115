9322bde3c7332fd7add211ac62d9781b85edfae4
bala-amypo <bala@amypo.in>
2025-12-15 12:56:44 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ShipmentRecordServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ShipmentRecordServiceImpl.java
@@ -1,16 +1,23 @@
 package com.example.demo.service.impl;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import com.example.demo.entity.ShipmentRecord;
 import com.example.demo.service.ShipmentRecordService;
-
+@Ser
 public class ShipmentRecordServiceImpl implements ShipmentRecordService{
     // createShipment(ShipmentRecord shipment);
     // updateShipmentStatus(long id,String status);
     // getShipmentByCode(String code);
     // getShipmentById(long id);
     // getAllShipments(); 
-    List<ShipmentRecord> sr = new ArrayList();
-    public ShipmentRecord 
+    List<ShipmentRecord> sr = new ArrayList<>();
+    int nextShipid=1;
+    public ShipmentRecord createShipment(ShipmentRecord shipment){
+           shipment.setId(nextShipid);
+           nextShipid++;
+           sr.add(shipment);
+           return shipment;
+    }
 }
