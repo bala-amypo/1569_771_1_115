25bfccd81f7415bd8bfdd93a06346f74918b68c7
bala-amypo <bala@amypo.in>
2025-12-15 10:36:44 +0000

    User commit

--- src/main/java/com/example/demo/entity/ShipmentRecord.java
+++ src/main/java/com/example/demo/entity/ShipmentRecord.java
@@ -1,9 +1,18 @@
 package com.example.demo.entity;
 
+import java.time.LocalDateTime;
+
 public class ShipmentRecord {
        private long id;
        private String shipmentCode;
        private String origin;
        private String destination;
-       private 
+       private String productType;
+       private LocalDateTime startDate;
+       private LocalDateTime expectedDelivery;
+       private String status;
+       private LocalDateTime createdAt;
+
+       public class ShipmentRecord(){}
+       public class ShipmentRecord(String shipmentCode,String origin,String destination,String productType,)
 }
