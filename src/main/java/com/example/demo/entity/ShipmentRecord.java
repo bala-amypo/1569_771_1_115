74ecfaf653db2206fd1263f35b10803fe51e626c
bala-amypo <bala@amypo.in>
2025-12-15 10:31:45 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/ShipmentRecord.java
@@ -0,0 +1,9 @@
+package com.example.demo.entity;
+
+public class ShipmentRecord {
+       private long id;
+       private String shipmentCode;
+       private String origin;
+       private String destination;
+       private 
+}
