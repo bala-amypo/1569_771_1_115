e5e489b483667156ad88d18e0e3c88a0a13715f3
bala-amypo <bala@amypo.in>
2025-12-15 12:26:44 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/BreachRecord.java
@@ -0,0 +1,9 @@
+package com.example.demo.entity;
+
+public class BreachRecord {
+    private long id;
+    private long shipmentId;
+    private long logId;
+    private String breachType;
+    pr
+}
--- src/main/java/com/example/demo/entity/TemperatureRule.java
+++ src/main/java/com/example/demo/entity/TemperatureRule.java
@@ -1,8 +1,81 @@
 package com.example.demo.entity;
 
+import java.time.LocalDate;
+
 public class TemperatureRule {
      private long id;
-     private long shipmentId;
-     private String sensorId;
-     private 
+     private String productType;
+     private Double minTemp;
+     private Double maxTemp;
+     private Boolean active;
+     private LocalDate effectiveFrom;
+     private LocalDate effectiveTo;
+
+     public TemperatureRule(){}
+
+    public TemperatureRule(String productType, Double minTemp, Double maxTemp, Boolean active, LocalDate effectiveFrom,
+            LocalDate effectiveTo) {
+        this.productType = productType;
+        this.minTemp = minTemp;
+        this.maxTemp = maxTemp;
+        this.active = active;
+        this.effectiveFrom = effectiveFrom;
+        this.effectiveTo = effectiveTo;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public Double getMinTemp() {
+        return minTemp;
+    }
+
+    public Double getMaxTemp() {
+        return maxTemp;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public LocalDate getEffectiveFrom() {
+        return effectiveFrom;
+    }
+
+    public LocalDate getEffectiveTo() {
+        return effectiveTo;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public void setMinTemp(Double minTemp) {
+        this.minTemp = minTemp;
+    }
+
+    public void setMaxTemp(Double maxTemp) {
+        this.maxTemp = maxTemp;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public void setEffectiveFrom(LocalDate effectiveFrom) {
+        this.effectiveFrom = effectiveFrom;
+    }
+
+    public void setEffectiveTo(LocalDate effectiveTo) {
+        this.effectiveTo = effectiveTo;
+    }
 }
