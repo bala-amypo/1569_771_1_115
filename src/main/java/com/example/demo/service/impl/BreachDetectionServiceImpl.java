676ebd05b0658b1021eb422c36f5082aacb2e140
bala-amypo <bala@amypo.in>
2025-12-16 15:53:54 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/BreachDetectionServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.demo.service.impl;
+import java.time.LocalDate;
+import java.util.List;
+
+import com.example.demo.entity.BreachRecord;
+import com.example.demo.service.BreachDetectionService;
+import com.example.demo.repository.BreachRecordRepository;
+
+@Service
+public TemperatureRuleServiceImpl implements TemperatureRuleService
