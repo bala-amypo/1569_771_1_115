d080ef423456b3314a9b7e6a54d673ebafb2f54c
bala-amypo <bala@amypo.in>
2025-12-16 10:12:35 +0000

    User commit

--- src/main/java/com/example/demo/service/ShipmentRecordService.java
+++ src/main/java/com/example/demo/service/ShipmentRecordService.java
@@ -1,15 +1,16 @@
 package com.example.demo.service;
 
+import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.ShipmentRecord;
 
 @Service
 public interface ShipmentRecordService {
-    createShipment(ShipmentRecord shipment);
-    updateShipmentStatus(long id,String status);
-    getShipmentByCode(String code);
-    getShipmentById(long id);
-    getAllShipments();
+    ShipmentRecord createShipment(ShipmentRecord shipment);
+    ShipmentRecord updateShipmentStatus(long id,String status);
+    ShipmentRecord getShipmentByCode(String code);
+    ShipmentRecord getShipmentById(long id);
+    List<ShipmentRecord> getAllShipments();
     
 }
