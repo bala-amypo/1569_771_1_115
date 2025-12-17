3f4f6df3f8d44cd4f4ed409bd12c48acab4291e9
bala-amypo <bala@amypo.in>
2025-12-17 08:49:47 +0000

    User commit

--- src/main/java/com/example/demo/controller/ShipmentRecordController.java
+++ src/main/java/com/example/demo/controller/ShipmentRecordController.java
@@ -7,45 +7,69 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.demo.entity.ShipmentRecord;
+import com.example.demo.entity.ShipmentRecord; 
 import com.example.demo.service.ShipmentRecordService;
 
 @RestController
 @RequestMapping("/api/shipments")
-public class ShipmentRecordController{
-    @Autowired 
-    ShipmentRecordService shipmentrecordservice;
+public class ShipmentRecordController {
+
+    @Autowired
+    ShipmentRecordService shipmentRecordService;
 
     @PostMapping
-    public ResponseEntity<ShipmentRecord> createShipment(@RequestBody ShipmentRecord shipment){
-        ShipmentRecord sh = shipmentrecordservice.createShipment(shipment);
-        return ResponseEntity.status(201).body(sh);
+    public ResponseEntity<ShipmentRecord> createShipment(@RequestBody ShipmentRecord shipment) {
+        ShipmentRecord sh = shipmentRecordService.createShipment(shipment);
+
+        if (sh != null) {
+            return ResponseEntity.status(201).body(sh);
+        } else {
+            return ResponseEntity.status(404).build();
+        }
+    }
+    @PutMapping("/{id}/status")
+    public ResponseEntity<ShipmentRecord> updateStatus(@PathVariable Long id,String status) {
+        ShipmentRecord sh = shipmentRecordService.updateShipmentStatus(id, status);
+
+        if (sh != null) {
+            return ResponseEntity.status(201).body(sh);
+        } else {
+            return ResponseEntity.status(404).build();
+        }
     }
 
-    @PutMapping
-    public ResponseEntity<ShipmentRecord> updateStatus(@PathVariable Long id,String status){
-        ShipmentRecord sh = shipmentrecordservice.updateShipmentStatus(id,status);
-        return ResponseEntity.status(201).body(sh);
-    }
-    
     @GetMapping("/code/{shipmentCode}")
-    public ResponseEntity<ShipmentRecord> getByCode(@PathVariable String shipmentCode){
-        ShipmentRecord sh = shipmentrecordservice.getShipmentByCode(shipmentCode);
-        return ResponseEntity.status(201).body(sh);
+    public ResponseEntity<ShipmentRecord> getByCode(@PathVariable String shipmentCode) {
+        ShipmentRecord sh = shipmentRecordService.getShipmentByCode(shipmentCode);
+
+        if (sh != null) {
+            return ResponseEntity.status(201).body(sh);
+        } else {
+            return ResponseEntity.status(404).build();
+        }
     }
-    
+
     @GetMapping("/{id}")
-    public ResponseEntity<ShipmentRecord> getById(@PathVariable Long id){
-        ShipmentRecord sh = shipmentrecordservice.getShipmentById(id);
-        return ResponseEntity.status(201).body(sh);
+    public ResponseEntity<ShipmentRecord> getById(@PathVariable Long id) {
+        ShipmentRecord sh = shipmentRecordService.getShipmentById(id);
+
+        if (sh != null) {
+            return ResponseEntity.status(201).body(sh);
+        } else {
+            return ResponseEntity.status(404).build();
+        }
     }
 
     @GetMapping
-    public List<ShipmentRecord> getAll(){
-        return shipmentrecordservice.getAllShipments();
+    public ResponseEntity<List<ShipmentRecord>> getAll() {
+        List<ShipmentRecord> list = shipmentRecordService.getAllShipments();
+        return ResponseEntity.status(201).body(list);
+        
     }
 }
