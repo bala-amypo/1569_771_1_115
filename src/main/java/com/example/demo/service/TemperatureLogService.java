43b053528c578b1ce7b2122d039967ca51643411
bala-amypo <bala@amypo.in>
2025-12-17 17:18:28 +0000

    User commit

--- src/main/java/com/example/demo/service/TemperatureLogService.java
+++ src/main/java/com/example/demo/service/TemperatureLogService.java
@@ -2,9 +2,10 @@ package com.example.demo.service;
 
 import java.util.List;
 import org.springframework.stereotype.Service;
+import com.example.demo.entity.TemperatureSensorLog;
+import com.example.demo.service.TemperatureLogService;
 
-import com.example.demo.entity.TemperatureLogService;
-
+@Service
 public interface TemperatureLogService{
     TemperatureSensorLog recordLog(TemperatureSensorLog log);
     List<TemperatureSensorLog> getLogsByShipment(long shipmentId);
