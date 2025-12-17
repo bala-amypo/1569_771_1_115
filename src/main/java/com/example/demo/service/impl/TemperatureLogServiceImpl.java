a2723905900e937715102135c9b6f49aed83b3c5
bala-amypo <bala@amypo.in>
2025-12-17 17:43:29 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/TemperatureLogServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/TemperatureLogServiceImpl.java
@@ -1,34 +1,41 @@
-package com.example.demo.service.impl;
+package com.example.demo1.service.impl;
 
 import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
-import com.example.demo.entity.TemperatureSensorLog;
-import com.example.demo.service.TemperatureLogService;
-import com.example.demo.repository.TemperatureSensorLogRepository;
+import com.example.demo1.entity.TemperatureSensorLog;
+import com.example.demo1.service.TemperatureLogService;
+import com.example.demo1.repository.TemperatureSensorLogRepository;
 
 @Service
-public class TemperatureLogServiceImpl implements TemperatureLogService{
+public class TemperatureLogServiceImpl implements TemperatureLogService {
+
     @Autowired
-    TemperatureSensorLogRepository temperaturelogrepository;
+    TemperatureSensorLogRepository temperatureLogRepository;
 
     @Override
-    public TemperatureSensorLog recordLog(TemperatureSensorLog log){
-        return temperaturelogrepository.save(log);
+    public TemperatureSensorLog recordLog(TemperatureSensorLog log) {
+        return temperatureLogRepository.save(log);
     }
 
     @Override
-    public List<TemperatureSensorLog> getLogsByShipment(long shipmentId){
-        return temperaturelogrepository.findByShipmentId(shipmentId);
+    public List<TemperatureSensorLog> getLogsByShipment(long shipmentId) {
+        List<TemperatureSensorLog> logs = temperatureLogRepository.findByShipmentId(shipmentId);
+        
+        if (logs.isEmpty()) {
+            throw new RuntimeException("No logs found for Shipment ID: " + shipmentId);
+        }
+        return logs;
     }
 
     @Override
-    public TemperatureSensorLog getLogById(long id){
-        return temperaturelogrepository.findById(id);
+    public TemperatureSensorLog getLogById(long id) {
+        return temperatureLogRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Temperature Log not found with ID: " + id));
     }
 
     @Override
-    public List<TemperatureSensorLog> getAllLogs(){
-        return temperaturelogrepository.findAll();
+    public List<TemperatureSensorLog> getAllLogs() {
+        return temperatureLogRepository.findAll();
     }
 }
