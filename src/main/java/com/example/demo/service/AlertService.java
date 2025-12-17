59ab735b19916ce0305e19d9b36987fd5a0eb105
bala-amypo <bala@amypo.in>
2025-12-17 04:15:11 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/AlertService.java
@@ -0,0 +1,13 @@
+package com.example.demo.service;
+import java.time.LocalDate;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.AlertRecord;
+
+@Service
+public interface AlertService{
+    AlertRecord triggerAlert(AlertRecord alert);
+    AlertRecord acknowledgeAlert(Long id);
+    List<AlertRecord> getAlertsByShipment(Long ship)
+}
