acb19f55fddc1943a3a3092f3a8e829f110bfc5d
bala-amypo <bala@amypo.in>
2025-12-18 00:14:37 +0000

    User commit

--- src/main/java/com/example/demo/entity/TemperatureSensorLog.java
+++ src/main/java/com/example/demo/entity/TemperatureSensorLog.java
@@ -6,7 +6,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
-import jakarta.persistence.Table;
 
 @Entity
 public class TemperatureSensorLog {
