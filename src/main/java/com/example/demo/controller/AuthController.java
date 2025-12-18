36e97b3bb582797dee52bae3b982cc6809b108cf
bala-amypo <bala@amypo.in>
2025-12-18 07:58:58 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,43 +1,43 @@
-// package com.example.demo.controller;
+package com.example.demo.controller;
 
-// import com.example.demo.entity.User;
-// import com.example.demo.service.UserService;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.PutMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-// @RestController
-// @RequestMapping("/auth")
-// public class AuthController {
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
 
-//     @Autowired
-//     UserService userService;
+    @Autowired
+    UserService userService;
    
-//     @PostMapping("/register")
-//     public ResponseEntity<String> register(@RequestBody User user){
-//         if (userService.findByEmail(user.getEmail()) != null) {
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody User user){
+        if (userService.findByEmail(user.getEmail()) != null) {
 
-//             return ResponseEntity.status(400).body("Email is already in use");
-//         } else {
-//             userService.registerUser(user);
-//             return ResponseEntity.status(201).body("User registered successfully");
-//         }
-//     }
+            return ResponseEntity.status(400).body("Email is already in use");
+        } else {
+            userService.registerUser(user);
+            return ResponseEntity.status(201).body("User registered successfully");
+        }
+    }
     
-//     @PostMapping("/login")
-//     public ResponseEntity<String> login(@RequestBody User loginUser) {
-//         User foundUser = userService.findByEmail(loginUser.getEmail());
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody User loginUser) {
+        User foundUser = userService.findByEmail(loginUser.getEmail());
 
-//         if (foundUser != null && loginUser.getPassword().equals(foundUser.getPassword())) {
-//             return ResponseEntity.status(200).body("Login successful");
-//         } else {
-//             return ResponseEntity.status(401).body("Invalid email or password");
-//         }
-//     }
-// }
+        if (foundUser != null && loginUser.getPassword().equals(foundUser.getPassword())) {
+            return ResponseEntity.status(200).body("Login successful");
+        } else {
+            return ResponseEntity.status(401).body("Invalid email or password");
+        }
+    }
+}
