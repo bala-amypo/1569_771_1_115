1ce22f613a33493c4641b499926d9599f70faebf
bala-amypo <bala@amypo.in>
2025-12-25 15:37:47 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,8 +1,6 @@
 package com.example.demo.controller;
 
-import com.example.demo.dto.AuthResponse;
-import com.example.demo.dto.LoginRequest;
-import com.example.demo.dto.RegisterRequest;
+import com.example.demo.dto.*;
 import com.example.demo.entity.User;
 import com.example.demo.security.JwtUtil;
 import com.example.demo.service.UserService;
@@ -15,32 +13,28 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final UserService userService;
-    private final AuthenticationManager authenticationManager;
+    private final AuthenticationManager authManager;
     private final JwtUtil jwtUtil;
 
     public AuthController(UserService userService,
-                          AuthenticationManager authenticationManager,
+                          AuthenticationManager authManager,
                           JwtUtil jwtUtil) {
         this.userService = userService;
-        this.authenticationManager = authenticationManager;
+        this.authManager = authManager;
         this.jwtUtil = jwtUtil;
     }
 
-    public ResponseEntity<AuthResponse> login(LoginRequest req) {
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest req) {
         User u = userService.findByEmail(req.getEmail());
         String token = jwtUtil.generateToken(u.getId(), u.getEmail(), u.getRole());
         return ResponseEntity.ok(new AuthResponse(token));
     }
 
-    public ResponseEntity<AuthResponse> register(RegisterRequest req) {
-        User u = new User();
-        u.setName(req.getFullName());
-        u.setEmail(req.getEmail());
-        u.setPassword(req.getPassword());
-
-        u = userService.registerUser(u);
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest req) {
+        User u = userService.registerUser(req.toUser());
         String token = jwtUtil.generateToken(u.getId(), u.getEmail(), u.getRole());
-
         return ResponseEntity.ok(new AuthResponse(token));
     }
 }
